55551b173ed3022b398f02ed880b33a1eba515f9
josephtheceo <159775398+josephkplp@users.noreply.github.com>
2024-09-19 06:51:25 +0000

    week4 assignment

---
+++ week4pyhton.py
@@ -0,0 +1,16 @@
+
+# Define the Person class
+class Person:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def introduce(self):
+        print(f"Hello, my name is {self.name}. I am {self.age} years old and my gender is {self.gender}.")
+
+# Create an instance of the Person class
+person_instance = Person(name="John Doe", age=30, gender="Male")
+
+# Call the introduce method to display the person's information
+person_instance.introduce()
